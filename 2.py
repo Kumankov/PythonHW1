979954a7cddbf8a1b7f6c9275c69b95e84cb9069
Ilya <kumankov@gmail.ru>
2022-12-12 21:55:22 +0300

    22

--- 2.py
+++ 2.py
@@ -2,8 +2,21 @@
 # утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для 
 # всех значений предикат.
 
+q=[0]*8
+i=0
+sum=0
+# result = 10 % 2 == 0
 for x in 0,1:
     for y in 0,1:
         for z in 0,1:
             print (f'X = {x}, Y = {y}, Z = {z}')
-            print(not(x or y or z) == (not (x) and not (y) and not (z)))     
+            q[i] = int(not(x or y or z) == (not (x) and not (y) and not (z)))
+            print(not(x or y or z) == (not (x) and not (y) and not (z)))            
+            sum=sum+q[i]
+            i+=1              
+if sum==0:
+    print('Выражение тождественно ложно') 
+elif sum==8:
+    print('Выражение тождественно истинно') 
+else: 
+    print('Выражение не является ни тождественно истинным ни тождественно ложным')
